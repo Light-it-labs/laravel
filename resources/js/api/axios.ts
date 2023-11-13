92feffeabad9b459d5d0688f431ecc334bd36acb
José Lin (Light-it) <jose.lin@lightit.io>
2023-11-13 00:07:21 -0300

    feat: axios instances

--- resources/js/api/axios.ts
+++ resources/js/api/axios.ts
@@ -1,19 +1,20 @@
 import axios from "axios";
 
 import { env } from "@/env";
-import { useUserStore } from "@/stores";
+import { errorResponse, privateRequest } from "./interceptors";
 
-export const api = axios.create({
+const baseConfig = {
   baseURL: env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
-});
-
-export const getAuthHeaders = () => {
-  const userToken = useUserStore.getState().token;
-
-  return {
-    Authorization: `Bearer ${userToken}`,
-  };
 };
+
+const publicAPI = axios.create(baseConfig);
+const privateAPI = axios.create(baseConfig);
+
+privateAPI.interceptors.request.use(privateRequest);
+privateAPI.interceptors.response.use((response) => response, errorResponse);
+
+export const getAPI = ({ isPrivate } = { isPrivate: true }) =>
+  isPrivate ? privateAPI : publicAPI;
--- resources/js/stores/useUserStore.ts
+++ resources/js/stores/useUserStore.ts
@@ -1,22 +1,36 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-interface UserStore {
-  token: string | null;
-
-  setToken: (token: string | null) => void;
+export interface User {
+  email: string;
+  name: string;
 }
 
-export const useUserStore = create<UserStore>()(
+export interface UserStoreState {
+  user: User | null;
+  token: string | null;
+  setUser: (user: User | null) => void;
+  setToken: (token: string | null) => void;
+  clearUser: () => void;
+}
+
+export const useUserStore = create<UserStoreState>()(
   persist(
     (set) => ({
+      user: null,
       token: null,
+      setUser: (user: User | null) => {
+        set(() => ({ user }));
+      },
       setToken: (token: string | null) => {
         set(() => ({ token }));
       },
+      clearUser: () => {
+        set({ user: null, token: null });
+      },
     }),
     {
-      name: "useUserStore",
+      name: "feedbackUserStore",
     },
   ),
 );
