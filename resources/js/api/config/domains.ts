4f12bc30148699c9364b21fe126499e44d9c11f1
JLin-99 <90731332+JLin-99@users.noreply.github.com>
2023-11-15 12:34:59 -0300

    update: frontend boilerplate (#9)
    * chore: app file rename
    
    * chore: app file rename
    
    * feat: frontend environment variables
    
    * feat: update env.example
    
    * feat: axios instances
    
    * feat: authentication
    
    * feat: add examples
    
    * feat: update PR template
    
    * fix: prettier
    
    * fix: overflow scroll
    
    * fix: .github/PULL_REQUEST_TEMPLATE.md
    
    * feat: add format options

--- resources/js/api/config/domains.ts
+++ resources/js/api/config/domains.ts
@@ -1,10 +1,5 @@
 export const DOMAINS = {
   project: "project",
-  permission: "permission",
-  client: "client",
-  assignment: "assignment",
-  employee: "employee",
-  team: "team",
   user: "user",
 } as const;
 export type Domain = keyof typeof DOMAINS;
@@ -12,7 +7,7 @@ export type Domain = keyof typeof DOMAINS;
 export type SubDomains<T extends string> = T extends Domain
   ? T
   : T extends `${infer First},${infer Rest}`
-  ? First extends Domain
-    ? `${First},${SubDomains<Rest>}`
-    : never
-  : never;
+    ? First extends Domain
+      ? `${First},${SubDomains<Rest>}`
+      : never
+    : never;
