d24713d81527373637ff3811323f73779957d12b
Juan Tejera <juan.tejera@lightit.io>
2024-01-17 16:07:40 -0300

    feat: change fallback background color for improved logo visibility

--- resources/js/screens/ErrorBoundaryFallback.tsx
+++ resources/js/screens/ErrorBoundaryFallback.tsx
@@ -1,10 +1,10 @@
-import { Logo } from "../../../public/build/assets";
+import { Logo } from "@/components";
 
 export const ErrorBoundaryFallback = () => {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="flex h-screen flex-col items-center justify-center bg-slate-800">
       <div className="mb-20">
-        <img src={Logo} alt="logo" />
+        <Logo />
       </div>
       <h2 className="mb-10 text-2xl">Something went terribly wrong!</h2>
     </main>
