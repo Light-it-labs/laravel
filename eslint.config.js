20fa0a32108facc2d50d4349dd769fc663571886
Naka <andres.nakanishi@lightit.io>
2024-09-12 10:49:35 -0300

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -11,102 +11,102 @@ import globals from "globals";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-    {
-        ignores: [
-            "**/eslint.config.mjs",
-            "**/*.config.js",
-            "**/*.config.cjs",
-            "node_modules/",
-            "**/vendor/",
-            "public/build/",
-            ".storybook/",
-            "**/reports/",
-        ],
-    },
-    ...fixupConfigRules(
-        compat.extends(
-            "eslint:recommended",
-            "plugin:@typescript-eslint/recommended-type-checked",
-            "plugin:@typescript-eslint/stylistic-type-checked",
-            "plugin:jsx-a11y/recommended",
-            "plugin:react-hooks/recommended",
-            "plugin:react/recommended",
-        ),
+  {
+    ignores: [
+      "**/eslint.config.mjs",
+      "**/*.config.js",
+      "**/*.config.cjs",
+      "node_modules/",
+      "**/vendor/",
+      "public/build/",
+      ".storybook/",
+      "**/reports/",
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "plugin:@typescript-eslint/stylistic-type-checked",
+      "plugin:jsx-a11y/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:react/recommended",
     ),
-    {
-        plugins: {
-            "@typescript-eslint": fixupPluginRules(typescriptEslint),
-            import: fixupPluginRules(_import),
-        },
-
-        linterOptions: {
-            reportUnusedDisableDirectives: true,
-        },
-
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.commonjs,
-                React: "writable",
-            },
-
-            parser: tsParser,
-            ecmaVersion: 5,
-            sourceType: "script",
-
-            parserOptions: {
-                project: true,
-            },
-        },
-
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-
-        rules: {
-            "react/prop-types": "off",
-            "@typescript-eslint/unbound-method": "off",
-
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                {
-                    argsIgnorePattern: "^_",
-                    varsIgnorePattern: "^_",
-                },
-            ],
-
-            "@typescript-eslint/consistent-type-imports": [
-                "warn",
-                {
-                    prefer: "type-imports",
-                    fixStyle: "separate-type-imports",
-                },
-            ],
-
-            "@typescript-eslint/no-misused-promises": [
-                2,
-                {
-                    checksVoidReturn: {
-                        attributes: false,
-                    },
-                },
-            ],
-
-            "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
-        },
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
     },
-    {
-        files: ["**/*.stories.*"],
-        rules: {
-            "react/react-in-jsx-scope": "off",
-        },
+
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
     },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+        React: "writable",
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "script",
+
+      parserOptions: {
+        project: true,
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "react/prop-types": "off",
+      "@typescript-eslint/unbound-method": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "separate-type-imports",
+        },
+      ],
+
+      "@typescript-eslint/no-misused-promises": [
+        2,
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+    },
+  },
+  {
+    files: ["**/*.stories.*"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
