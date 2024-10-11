d396bf86a2ffde4f2272e533c0a07f6b780538c7
sgoycoechea-lightit <santiago.goycoechea@lightit.io>
2024-10-11 15:16:22 -0300

    feat: remove modal routes

--- resources/js/screens/Home.tsx
+++ resources/js/screens/Home.tsx
@@ -1,32 +1,37 @@
-import { MODAL_ROUTES } from "@/router";
-import { useNavigateModal } from "@/router/useNavigateModal";
+import React from "react";
+
+import { ExampleModal } from "@/modals";
 import { Button } from "@/ui";
 
 export const Home = () => {
-  const navigateModal = useNavigateModal();
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
   return (
-    <div className="prose p-10 text-white lg:prose-xl">
-      <h1 className="text-white">HOME Title</h1>
+    <>
+      <div className="prose p-10 text-white lg:prose-xl">
+        <h1 className="text-white">HOME Title</h1>
 
-      <h2 className="text-white">HOME Subtitle</h2>
+        <h2 className="text-white">HOME Subtitle</h2>
 
-      <h3 className="text-white">HOME SubSubtitle</h3>
+        <h3 className="text-white">HOME SubSubtitle</h3>
 
-      <p>paragraph</p>
+        <p>paragraph</p>
 
-      <ul>
-        <li>item</li>
+        <ul>
+          <li>item</li>
 
-        <li>item</li>
+          <li>item</li>
 
-        <li>item</li>
+          <li>item</li>
 
-        <li>item</li>
-      </ul>
+          <li>item</li>
+        </ul>
 
-      <Button onClick={() => navigateModal(MODAL_ROUTES.exampleModal)}>
-        I open the example modal
-      </Button>
-    </div>
+        <Button onClick={() => setIsModalOpen(true)}>
+          I open the example modal
+        </Button>
+      </div>
+      <ExampleModal show={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
   );
 };
